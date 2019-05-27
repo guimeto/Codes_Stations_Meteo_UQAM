76f4b0c3ef523b6e3bdf4fc30229d9e8bc2b6834
Dueymes <guillaume.dueymes@gmail.com>
2019-05-27 11:08:17 -0400

    Modification IP

--- Preparation_DataBase/stationlib/bd.py
+++ Preparation_DataBase/stationlib/bd.py
@@ -1,10 +1,10 @@
 # -*-coding:Latin-1 -*
 import mysql.connector
 def bd(releveMeteoQuery):
-        cnx = mysql.connector.connect(host = '132.208.132.40', 
-                                      database = 'BD_labostation__enquete', 
-                                      user = 'station', 
-                                      password = 'reli39,cao') 
+        cnx = mysql.connector.connect(host = 'XXX.XXX.XXX.XX', 
+                                      database = 'database_name', 
+                                      user = 'name', 
+                                      password = 'password') 
 
 #Création d'un curseur et exécution de la requête SQL passée en paramètre
 	cursor = cnx.cursor ()
